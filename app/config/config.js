8c0a7166e28f22107cd9361c119a32486691173f
Marcos <marcos@ciclo.tech>
2022-11-02 10:48:13 -0600

    cambios m

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    //DB: process.env.DB || 'mongodb://localhost:27017/hrocas'
     DB: process.env.DB || 'mongodb+srv://JROBS:jROBERTO12@cluster0.gdmlv3w.mongodb.net/pollos?retryWrites=true&w=majority'
 }
